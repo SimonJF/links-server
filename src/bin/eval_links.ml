c561e02b127c35d6a2c9553ca8873af9c578a4a6
TomJDavey <s1688185@ed.ac.uk>
2019-06-19 14:25:53 +0100

    Attempt at modifying evaluation

--- src/bin/eval_links.ml
+++ src/bin/eval_links.ml
@@ -5,7 +5,7 @@ open Links_core.Sugartypes
 (* Parses a string and returns a Links expression *)
 (* TODO: Exception handling *)
 let parse str =
-  Parse.parse_string Parse.interactive str
+  Parse.parse_file Parse.program str
 
 (* Takes a REPL string and produces a pair of perhaps a Links value (in the case of an expression)
  * and updated environments. *)
@@ -16,6 +16,15 @@ let evaluate str envs : (Value.t option * Driver.evaluation_env) =
    * an expression, and updated environments. *)
   let evaluate_parse_result envs parse_result =
     let _, nenv, tyenv = envs in
+    let (defs, e, nenv'), tyenv' = parse_result in
+    let valenv, v = Driver.process_program true envs e [] in
+    let updated_envs =
+      (valenv,
+       Env.String.extend nenv nenv',
+       Types.extend_typing_environment tyenv tyenv') in
+    (v, updated_envs) in
+
+    (*
     match parse_result with
       | `Definitions (defs, nenv'), tyenv' ->
           (* Process each definition in turn. *)
@@ -33,13 +42,17 @@ let evaluate str envs : (Value.t option * Driver.evaluation_env) =
           let (_, nenv, tenv) = envs in
           let updated_envs = (valenv, nenv, tenv) in
           (Some v, updated_envs) in
+          *)
 
   let expr, pos_context = parse str in
   let (_, nenv, tyenv) = envs in
   (* Begin by parsing the input string. *)
-  let sentence, sentence_ty, tyenv' =
-    Frontend.Pipeline.interactive tyenv pos_context expr in
-  let sentence = match sentence with
+  let (program, program_ty, tyenv'), _ =
+    Frontend.Pipeline.program tyenv pos_context expr in
+  let program =
+    let tenv = Var.varify_env (nenv, tyenv.Types.var_env) in
+    Sugartoir.desugar_program (nenv, tenv, tyenv.Types.effect_row) program in
+    (*
     | Definitions defs ->
         let tenv = Var.varify_env (nenv, tyenv.Types.var_env) in
         let defs, nenv' = Sugartoir.desugar_definitions (nenv, tenv, tyenv.Types.effect_row) defs in
@@ -47,14 +60,14 @@ let evaluate str envs : (Value.t option * Driver.evaluation_env) =
     | Expression e     ->
         let tenv = Var.varify_env (nenv, tyenv.Types.var_env) in
         let e = Sugartoir.desugar_expression (nenv, tenv, tyenv.Types.effect_row) e in
-        `Expression (e, sentence_ty)
+        `Expression (e, program_ty)
     (* SJF: The Links REPL allows "directives" which do things like loading files and such.
      * A lot of these could be used to do insecure things. I'm disabling them all for now,
      * but it might make sense to whitelist some useful-yet-safe ones later. *)
     | Directive _      -> raise (Failure ("Directives not allowed.")) in
-  evaluate_parse_result envs (sentence, tyenv')
+    *)
+  evaluate_parse_result envs (program, tyenv')
 
 let init () =
   let (_prelude, envs) = Driver.NonInteractive.load_prelude () in
   envs
-  
